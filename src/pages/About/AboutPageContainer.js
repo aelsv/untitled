c922fee06c4f5761eb51938a566a01bb859fa9f9
Alexandr <alexander.elsv@gmail.com>
2021-05-27 21:35:57 +0300

    UN-16 – added HomePage, AboutPage & NotFound Page

---
+++ src/pages/About/AboutPageContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AboutPageComponent from 'pages/About/AboutPageComponent';
+
+export const AboutPageContainer = () => <AboutPageComponent />;
+
+AboutPageContainer.displayName = 'AboutPageContainer';
---
+++ src/pages/NotFound/NotFoundComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundComponent = () => (
+  <div>
+    <h1>404!</h1>
+  </div>
+);
+
+NotFoundComponent.displayName = 'NotFoundComponent';
+
+export default NotFoundComponent;
